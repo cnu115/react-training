11062bbd50e6f8192f2b1a9d3c392e2d007878c6
m_srinivas <cnu.prince8@gmail.com>
2024-06-02 11:17:03 +0530

    registartion functionality

--- src/Auth/registration.js
+++ src/Auth/registration.js
@@ -1,11 +1,34 @@
 import React, { useState } from "react";
 import Nav from "../Layouts/Nav";
 import { Button, Card, Container, Form } from "react-bootstrap";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 const Registration = () => {
 
-
+    const navigate = useNavigate();
     const [validated, setValidated] = useState(false);
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [formValues, setFormValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '9823212323',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleInputChange = (event) => {
+        // debugger;
+        const { name, value } = event.currentTarget;
+        const oldFormValues = { ...formValues }; //spread operator
+        oldFormValues[name] = value;
+        // console.log('formValues', oldFormValues)
+        setFormValues(oldFormValues);
+    }
+
+    // console.log('formValues', formValues)
 
     const handleSubmit = (event) => {
         // debugger;
@@ -15,26 +38,46 @@ const Registration = () => {
             event.stopPropagation();
         }
         setValidated(true);
+        
+        const {firstName, lastName, confirmPassword, email, phone, password} = formValues;
+        if(firstName !== '' && lastName !== ''  && email !== ''  && phone !== ''  && password !== '' && confirmPassword !== ''){
+            createUsersApi();
+        }
     }
 
-    //     "firstName": "Emily",
-    //   "lastName": "Johnson",
-    //   "maidenName": "Smith",
-    //   "age": 28,
-    //   "gender": "female",
-    //   "email": "emily.johnson@x.dummyjson.com",
-    //   "phone": "+81 965-431-3024"
+    const createUsersApi = () => {
+        axios.post('https://dummyjson.com/users/add', formValues, {
+             'Content-Type': 'application/json' 
+        })
+        .then((response) => {
+            if(response.status === 201){
+                alert('register successful');
+                setTimeout(()=>{
+                    navigate('/login');
+                },200);
+            }else{
+                setError(true);
+                setErrorMessage(response.statusText)
+            }
+        })
+        .catch((error) => {
+            setError(true)
+            setErrorMessage(error.message)
+        });
+    }
+
     return (
         <div>
             <Nav />
             <Container className="form-container">
+                {error && errorMessage !== '' && <span>{errorMessage}</span>}
                 <Card>
                     <Card.Header>Registration</Card.Header>
                     <Card.Body>
                         <Form noValidate={true} validated={validated} onSubmit={(event) => handleSubmit(event)}>
                             <Form.Group className="mb-3" controlId="validationFirstName" >
                                 <Form.Label>First Name</Form.Label>
-                                <Form.Control required name="firstName" type="text" placeholder="John" />
+                                <Form.Control onChange={(event) => handleInputChange(event)} required name="firstName" type="text" placeholder="John" />
                                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                                 <Form.Control.Feedback type="invalid">
                                     Please enter first name.
@@ -42,26 +85,37 @@ const Registration = () => {
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Last Name</Form.Label>
-                                <Form.Control required name="lastName" type="text" placeholder="Smith" />
+                                <Form.Control onChange={(event) => handleInputChange(event)} required name="lastName" type="text" placeholder="Smith" />
                                 <Form.Control.Feedback type="invalid">
                                     Please enter second name.
                                 </Form.Control.Feedback>
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Email Address</Form.Label>
-                                <Form.Control type="email" placeholder="name@example.com" />
+                                <Form.Control onChange={(event) => handleInputChange(event)} name="email" type="email" placeholder="name@example.com" />
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Mobile Number</Form.Label>
-                                <Form.Control name="phone" type="number" placeholder="+91723456789" />
+                                <Form.Control onChange={(event) => handleInputChange(event)} name="phone" value={formValues.phone} type="number" placeholder="+91723456789" />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control name="password" type="password" placeholder="************" />
+                                <Form.Control required onChange={(event) => handleInputChange(event)} name="password" type="password" placeholder="************" />
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter password.
+                                </Form.Control.Feedback>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                                 <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control name="confirmPassword" type="password" placeholder="************" />
+                                <Form.Control required onChange={(event) => handleInputChange(event)} name="confirmPassword"
+                                    type="password" placeholder="************"
+                                    isInvalid={
+                                        validated &&
+                                        formValues.confirmPassword !== formValues.password
+                                    } />
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter confirm password.
+                                </Form.Control.Feedback>
                             </Form.Group>
                             <Button type="submit" variant="primary">Register</Button>
                         </Form>
