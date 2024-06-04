3173f838a92fb0eed3e67e4edf14549de9627c64
m_srinivas <cnu.prince8@gmail.com>
2024-06-04 08:45:07 +0530

    login functionality

--- src/Auth/login.js
+++ src/Auth/login.js
@@ -1,9 +1,62 @@
-import React from "react"
+import React, { useState } from "react"
 import Form from 'react-bootstrap/Form';
 import Nav from "../Layouts/Nav";
 import { Button, Card, Container } from "react-bootstrap";
+import axios from "axios";
 
 const Login = () => {
+    const [validated, setValidated] = useState(false);
+
+    const [formValues, setFormValues] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleFormSubmit = (event) => {
+        debugger;
+        event.preventDefault()
+        const target = event.currentTarget;
+        if (target.checkValidity() === false) {
+            event.stopPropagation();
+        }
+        setValidated(true);
+
+        if (formValues.username !== "" && formValues.password !== "") {
+            handleApi();
+        }
+    }
+
+    const handleInputChange = (event) => {
+
+        const { name, value } = event.target;
+
+        const oldFormValues = { ...formValues }
+        oldFormValues[name] = value;
+
+        setFormValues(oldFormValues);
+
+
+        // setFormValues({
+        //     ...formValues,
+        //     [name]: value
+        // });
+
+    }
+
+    const handleApi = () => {
+        axios.post('https://dummyjson.com/auth/login', formValues, {
+            'Content-Type': 'application/json'
+        }).then(res => {
+            console.log('res', res);
+            if (res.status === 200) {
+                const { token } = res.data;
+                sessionStorage.setItem('token', token);
+            }
+        }).catch(error => {
+            console.error('error', error);
+        })
+    }
+
     return (
         <>
             <Nav />
@@ -11,16 +64,30 @@ const Login = () => {
                 <Card>
                     <Card.Header>Login</Card.Header>
                     <Card.Body>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form noValidate validated={validated} onSubmit={(event) => handleFormSubmit(event)}>
+                            {/* 2-way binding */}
+                            {/* <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="name@example.com" />
+                                <Form.Control required type="email" name='email' placeholder="name@example.com" onChange={(event) => handleInputChange(event)} />
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter email.
+                                </Form.Control.Feedback>
+                            </Form.Group> */}
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control required type="text" name='username' placeholder="username" onChange={(event) => handleInputChange(event)} />
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter username.
+                                </Form.Control.Feedback>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="************" />
+                                <Form.Control required type="password" name="password" placeholder="************" onChange={(event) => handleInputChange(event)} />
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter password.
+                                </Form.Control.Feedback>
                             </Form.Group>
-                            <Button variant="primary">Login</Button>
+                            <Button type="submit" variant="primary">Login</Button>
                         </Form>
                     </Card.Body>
                 </Card>
